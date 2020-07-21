977db74367e1f6aad1b72442279b550e22741904
Ojhal Patwa <patkarojhal26@gmail.com>
2020-07-21 18:42:36 +0530

    first commit

---
+++ src/Demo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import ReactDom from 'react-dom';
+
+class Demo extends Component {
+  render() {
+    return <h1> Hello World</h1>
+  }
+}
+
+export default Demo;
