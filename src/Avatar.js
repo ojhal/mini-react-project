0b9131a720f6e767b75dcd65bb0ba52ad588e46e
Ojhal Patwa <patkarojhal26@gmail.com>
2020-07-22 19:20:13 +0530

    use class component

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,55 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Avatar.css';
 import 'tachyons';
 
 import Avatarlist from './Avatarlist';
-const Avatar = (props) => {
+class Avatar extends Component {
+  render() {
+    const Avatarlistarray = [
+      {
+        id: 1,
+        name: "Jyoti",
+        work: "Web developer"
+      },
+      {
+        id: 2,
+        name: "Sonam",
+        work: "Programmer"
+      },
+      {
+        id: 3,
+        name: "SonamTechnical",
+        work: "FrontEnd developer"
+      },
+      {
+        id: 4,
+        name: "JyotiTechnical",
+        work: "BackEnd developer"
+      }
+    ]
 
-  const Avatarlistarray = [
-    {
-      id: 1,
-      name: "Jyoti",
-      work: "Web developer"
-    },
-    {
-      id: 2,
-      name: "Sonam",
-      work: "Programmer"
-    },
-    {
-      id: 3,
-      name: "SonamTechnical",
-      work: "FrontEnd developer"
-    },
-    {
-      id: 4,
-      name: "JyotiTechnical",
-      work: "BackEnd developer"
-    }
-  ]
+    const avatararraycard = Avatarlistarray.map((avatarcard, i) => {
+      return <Avatarlist id={Avatarlistarray[i].name}
+        name={Avatarlistarray[i].name}
+        work={Avatarlistarray[i].work} />
 
-  return (
-    <div>
-      <h1> Welcome to Avatar World</h1>
-      <Avatarlist id={Avatarlistarray[0].name}
-        name={Avatarlistarray[0].name}
-        work={Avatarlistarray[0].work} />
+    })
 
-      <Avatarlist id={Avatarlistarray[1].name}
-        name={Avatarlistarray[1].name}
-        work={Avatarlistarray[1].work} />
-
-      <Avatarlist id={Avatarlistarray[2].name}
-        name={Avatarlistarray[2].name}
-        work={Avatarlistarray[2].work} />
-
-      <Avatarlist id={Avatarlistarray[3].name}
-        name={Avatarlistarray[3].name}
-        work={Avatarlistarray[3].work} />
-
-      <button>Subscribe</button>
-    </div>
-  )
+    return (
+      <div className="design">
+        <h1> Welcome to Avatar World</h1>
+        {avatararraycard}
+        <button>Subscribe</button>
+      </div>
+    )
+  }
 }
 
 
--- src/Avatarlist.js
+++ src/Avatarlist.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Avatarlist = (props) => {
   return (
-    <div className="avatarstyle ma4 bg-light-blue dib pa3 tc">
+    <div className="avatarstyle ma4 bg-light-blue dib pa3 grow tc">
       <img src={`https://joeschmoe.io/api/v1/${props.name}`} alt="Avatar" />
       <h1 className="">{props.name}</h1>
       <p>{props.work}</p>
